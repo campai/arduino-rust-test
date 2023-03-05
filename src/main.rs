641261c63dea75bc9e0ee983da22489dd5996d7c
campai <the.campai@gmail.com>
2023-03-05 12:12:01 +0100

    feat: blink L13 on arduino uno

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    let peripherals = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(peripherals);
+    let mut led_13 = pins.d13.into_output();
+
+    loop {
+        led_13.toggle();
+
+        arduino_hal::delay_ms(100);
+    }
+}
